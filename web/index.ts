46ca195cf9bbad85144fcdf5c919c1e01d5b75fb
aboutDavid <62346025+aboutDavid@users.noreply.github.com>
2020-11-05 11:30:32 -0500

    Add me lol

--- web/index.ts
+++ web/index.ts
@@ -145,6 +145,7 @@ const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Me', value: 'https://file.coffee/u/yp5bulkAGn.png' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
@@ -152,6 +153,7 @@ const imageDarkOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Me', value: 'https://file.coffee/u/yp5bulkAGn.png' },
 ];
 
 const widthOptions = [

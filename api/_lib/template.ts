d2a125efbe09054d6c82c02366c5cf67c0ebcd38
aboutDavid <62346025+aboutDavid@users.noreply.github.com>
2020-11-05 10:54:25 -0500

    See what happens when you remove the logo

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -117,14 +117,12 @@ export function getHtml(parsedReq: ParsedRequest) {
         <div>
             <div class="spacer">
             <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
-            </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
+                ${emojify(
                 md ? marked(text) : sanitizeHtml(text)
             )}
+            </div>
+            <div class="spacer">
+            <div class="heading">
             </div>
         </div>
     </body>
